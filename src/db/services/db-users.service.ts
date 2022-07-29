b62ef27fb9de56d3565e5fbbb9bf3aac72c7bb00
Bogdan <->
2022-07-30 02:55:20 +0300

    feat: add db-users service

---
+++ src/db/services/db-users.service.ts
@@ -0,0 +1,129 @@
+import {
+  Injectable,
+  BadRequestException,
+  BadGatewayException,
+  InternalServerErrorException,
+} from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
+import { CreateUserDto } from 'src/users/dto/create-user.dto';
+import { UpdateUserDto } from 'src/users/dto/update-user.dto';
+import { Prisma } from '@prisma/client';
+
+@Injectable()
+export class DbUsers {
+  private readonly filteredUserSelect = {
+    id: true,
+    login: true,
+    password: true,
+    age: true,
+  };
+
+  constructor(private readonly prisma: PrismaService) {}
+
+  private catchPrismaError(error: { code: string }): never {
+    if (error instanceof Prisma.PrismaClientRustPanicError) {
+      process.exit(1);
+    } else if (error instanceof Prisma.PrismaClientValidationError) {
+      throw new InternalServerErrorException(
+        `Attempt to use invalid value in database.`,
+      );
+    } else if (error instanceof Prisma.PrismaClientKnownRequestError) {
+      if (error.code === 'P2002') {
+        throw new BadRequestException('Unique constraint failed.');
+      } else {
+        throw new BadGatewayException('Database related error occurred.');
+      }
+    } else {
+      throw new InternalServerErrorException();
+    }
+  }
+
+  findManyByLogin(limit: number, loginSubstring: string) {
+    return this._findManyByLogin(limit, loginSubstring).catch(
+      this.catchPrismaError,
+    );
+  }
+
+  findOneById(id: string) {
+    return this._findOneById(id).catch(this.catchPrismaError);
+  }
+
+  create(createUserDto: CreateUserDto) {
+    return this._create(createUserDto).catch(this.catchPrismaError);
+  }
+
+  updateById(id: string, updateUserDto: UpdateUserDto) {
+    return this._updateById(id, updateUserDto).catch(this.catchPrismaError);
+  }
+
+  deleteById(id: string) {
+    return this._deleteById(id).catch(this.catchPrismaError);
+  }
+
+  private async _findManyByLogin(limit: number, loginSubstring: string) {
+    const users = await this.prisma.user.findMany({
+      take: limit,
+      orderBy: { login: 'asc' },
+      where: {
+        isDeleted: false,
+        login: { startsWith: loginSubstring },
+      },
+      select: this.filteredUserSelect,
+    });
+
+    return users;
+  }
+
+  private async _findOneById(id: string) {
+    const user = await this.prisma.user.findUnique({
+      where: {
+        id_isDeleted: {
+          id: id,
+          isDeleted: false,
+        },
+      },
+      select: this.filteredUserSelect,
+    });
+
+    return user;
+  }
+
+  private async _create({ login, password, age }: CreateUserDto) {
+    const newUser = await this.prisma.user.create({
+      data: { login, password, age },
+      select: this.filteredUserSelect,
+    });
+
+    return newUser;
+  }
+
+  private async _updateById(id: string, updateUserDto: UpdateUserDto) {
+    const updatedUser = await this.prisma.user.update({
+      where: {
+        id_isDeleted: {
+          id: id,
+          isDeleted: false,
+        },
+      },
+      data: updateUserDto,
+      select: this.filteredUserSelect,
+    });
+
+    return updatedUser;
+  }
+
+  private async _deleteById(id: string) {
+    const deletedUser = await this.prisma.user.update({
+      where: {
+        id_isDeleted: {
+          id: id,
+          isDeleted: false,
+        },
+      },
+      data: { isDeleted: true },
+      select: this.filteredUserSelect,
+    });
+
+    return deletedUser;
+  }
+}

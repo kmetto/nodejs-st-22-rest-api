5cc8aff50827ba4d09d14f241ee8ec169088c4e8
Bogdan <->
2022-07-30 02:56:47 +0300

    feat: change users service to use db

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,71 +1,34 @@
-import { randomUUID } from 'node:crypto';
 import { Injectable } from '@nestjs/common';
-import { User } from './entities/user.entity';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { DbUsers } from 'src/db/services/db-users.service';
 
 @Injectable()
 export class UsersService {
-  users: User[] = [];
+  constructor(private readonly dbUsers: DbUsers) {}
 
-  findMany(limit: number, loginSubstring: string) {
-    const selectedUsers: User[] = [];
-
-    this.users.some((user) => {
-      const isEnd = selectedUsers.length >= limit;
-      if (isEnd) return true;
-
-      const isSubstring = user.login.startsWith(loginSubstring);
-      if (isSubstring && !user.isDeleted) {
-        selectedUsers.push(user);
-      }
-    });
-
-    return selectedUsers;
+  async findManyByLogin(limit: number, loginSubstring: string) {
+    const users = await this.dbUsers.findManyByLogin(limit, loginSubstring);
+    return users;
   }
 
-  findOne(
-    selector: string,
-    { selectorType }: { selectorType: 'id' | 'login' },
-  ) {
-    const selectedUser =
-      this.users.find((user) => user[selectorType] === selector) ?? null;
-
-    if (selectedUser?.isDeleted) return null;
-
-    return selectedUser;
+  async findOneById(id: string) {
+    const user = await this.dbUsers.findOneById(id);
+    return user;
   }
 
-  createOne(createUserDto: CreateUserDto) {
-    const newUser: User = {
-      id: randomUUID(),
-      isDeleted: false,
-      ...createUserDto,
-    };
-    this.users.push(newUser);
-
+  async create(createUserDto: CreateUserDto) {
+    const newUser = await this.dbUsers.create(createUserDto);
     return newUser;
   }
 
-  updateOne(id: string, updateUserDto: UpdateUserDto) {
-    const updatedUser = this.findOne(id, { selectorType: 'id' });
-
-    if (updatedUser === null) return null;
-
-    for (const key of Object.keys(updateUserDto)) {
-      updatedUser[key] = updateUserDto[key];
-    }
-
+  async updateById(id: string, updateUserDto: UpdateUserDto) {
+    const updatedUser = await this.dbUsers.updateById(id, updateUserDto);
     return updatedUser;
   }
 
-  deleteOne(id: string) {
-    const deletedUser = this.findOne(id, { selectorType: 'id' });
-
-    if (deletedUser === null) return null;
-
-    deletedUser.isDeleted = true;
-
+  async deleteById(id: string) {
+    const deletedUser = await this.dbUsers.deleteById(id);
     return deletedUser;
   }
 }

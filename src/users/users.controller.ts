af07c224733851562bf7f71c7bb57384a56533bb
Bogdan <->
2022-07-30 02:57:36 +0300

    feat: change users controller to use db

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -23,75 +23,45 @@ export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Get()
-  findMany(
+  async findMany(
     @Query('limit', new DefaultValuePipe(100), ParseIntPipe) limit: number,
     @Query('loginSubstring', new DefaultValuePipe(''))
     loginSubstring: string,
   ) {
-    const users = this.usersService.findMany(limit, loginSubstring);
-
+    const users = await this.usersService.findManyByLogin(
+      limit,
+      loginSubstring,
+    );
     return users;
   }
 
   @Get(':id')
-  findOne(@Param('id', ParseUUIDPipe) id: string) {
-    const user = this.usersService.findOne(id, { selectorType: 'id' });
-
-    if (user === null) throw new NotFoundException();
-
+  async findOne(@Param('id', ParseUUIDPipe) id: string) {
+    const user = await this.usersService.findOneById(id);
     return user;
   }
 
   @Post()
-  createOne(
+  async createOne(
     @Body(CustomValidationPipe)
     createUserDto: CreateUserDto,
   ) {
-    this.validateLogin(createUserDto.login, undefined, {
-      passUnchangedLogin: false,
-    });
-
-    const newUser = this.usersService.createOne(createUserDto);
+    const newUser = await this.usersService.create(createUserDto);
     return newUser;
   }
 
   @Put(':id')
-  updateOne(
+  async updateOne(
     @Param('id', ParseUUIDPipe) id: string,
     @Body(CustomValidationPipe) updateUserDto: UpdateUserDto,
   ) {
-    this.validateLogin(updateUserDto.login, id, { passUnchangedLogin: true });
-
-    const updatedUser = this.usersService.updateOne(id, updateUserDto);
-
-    if (updatedUser === null) throw new NotFoundException();
-
+    const updatedUser = await this.usersService.updateById(id, updateUserDto);
     return updatedUser;
   }
 
   @Delete(':id')
-  deleteOne(@Param('id', ParseUUIDPipe) id: string) {
-    const deletedUser = this.usersService.deleteOne(id);
-
-    if (deletedUser === null) throw new NotFoundException();
-
+  async deleteOne(@Param('id', ParseUUIDPipe) id: string) {
+    const deletedUser = await this.usersService.deleteById(id);
     return deletedUser;
-  }
-
-  private validateLogin(
-    newLogin: string,
-    currentId = '',
-    { passUnchangedLogin }: { passUnchangedLogin: boolean },
-  ) {
-    const userByLogin = this.usersService.findOne(newLogin, {
-      selectorType: 'login',
-    });
-
-    if (userByLogin === null) return;
-
-    const isLoginStayedSame = userByLogin?.id === currentId;
-    if (passUnchangedLogin && isLoginStayedSame) return;
-
-    throw new BadRequestException('The suggested login is already in use');
   }
 }

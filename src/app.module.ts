61bfd211f62b76a52eb64d43dd8d26859dee246a
Bogdan <->
2022-07-30 02:56:09 +0300

    feat: import db module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
+import { DbModule } from './db/db.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [DbModule, UsersModule],
 })
 export class AppModule {}

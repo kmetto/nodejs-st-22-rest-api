4c74d6993e48311a6c96aedf832f83315da7b065
Bogdan <->
2022-07-30 17:14:09 +0300

    feat: use built-in ValidationPipe

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,24 @@
-import { VersioningType } from '@nestjs/common';
+import { VersioningType, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      forbidUnknownValues: true,
+    }),
+  );
+
   app.enableVersioning({
     type: VersioningType.URI,
     defaultVersion: '1',
   });
+
   await app.listen(3000);
 }
+
 bootstrap();
